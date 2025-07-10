f1cd1b3d74ba0d6307146dc71a1db17c026e5d0c
Ammaron <ammaron99@gmail.com>
2025-07-09 22:02:32 -0500

    assignment 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        bool end = false;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            string numberString = Console.ReadLine();
+            int number = int.Parse(numberString);
+
+            if (number == 0)
+            {
+                end = true;
+            }
+            else
+            {
+                numbers.Add(number);
+            }
+        } while (end != true);
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+        int sum = numbers.Sum();
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
     }
 }
