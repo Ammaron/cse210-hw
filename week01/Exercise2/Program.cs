2baf1ae3db59af07b8ab2329abe7773a020d6b0a
Ammaron <ammaron99@gmail.com>
2025-07-09 20:14:45 -0500

    Assignment 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        string letter = "A";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent < 90 && percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent < 80 && percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent < 70 && percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percent >= 70 && letter != "A")
+        {
+            Console.WriteLine($"Congrats, you pass! You scored with a {letter}");
+        }
+        else if (letter == "A")
+        {
+            Console.WriteLine($"Congrats, you pass! You scored with an {letter}");
+        }
+        else if (letter == "F")
+        {
+            Console.WriteLine($"You failed with an {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"You failed with a {letter}");
+        }
     }
 }
