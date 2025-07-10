ea2b1b3c45a7756415f59d4683f22813ada5a51d
Ammaron <ammaron99@gmail.com>
2025-07-09 21:30:18 -0500

    Week 1 assignment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
+        int numberGuess = 99;
+        int count = 1;
+        do
+        {
+            Console.WriteLine("What is guess for the magic number?");
+            string numberGuessString = Console.ReadLine();
+            numberGuess = int.Parse(numberGuessString);
+
+            if (numberGuess == magicNumber)
+            {
+                Console.WriteLine($"You got it in {count} turns!");
+            }
+            else if (numberGuess > magicNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (numberGuess < magicNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+            }
+            count++;
+        } while (numberGuess != magicNumber);
     }
 }
